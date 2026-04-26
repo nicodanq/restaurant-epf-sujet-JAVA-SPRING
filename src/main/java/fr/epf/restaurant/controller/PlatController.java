afb8dc1784cf8ff21314b43ad864ff0c96ff9f20
nicolas.danquigny <nicolas@trixie>
2026-04-26 18:39:18 +0200

    fix(controller): ajout @PathVariable sur findById de PlatController

--- src/main/java/fr/epf/restaurant/controller/PlatController.java
+++ src/main/java/fr/epf/restaurant/controller/PlatController.java
@@ -3,6 +3,7 @@ package fr.epf.restaurant.controller;
 import fr.epf.restaurant.dao.PlatDao;
 import fr.epf.restaurant.model.Plat;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -21,5 +22,10 @@ public class PlatController {
     @GetMapping
     public List<Plat> findAll() {
         return platDao.findAll();
+    }
+
+    @GetMapping("/{id}")
+    public Plat findById(@PathVariable Long id) {
+        return platDao.findById(id);
     }
 }
